ad11fdc28d43dc31d35009f0ff1db4683760c7e4
Artem <linequantum@gmail.com>
2026-04-23 10:13:21 +0300

    Add prices for computer mice

--- mice-data.js
+++ mice-data.js
@@ -2,27 +2,27 @@ const miceProducts = [
   {
     name: "Ігрова компьютерная Мишка Gaming 6 кнопок для професійних гравців в оригінальній подарунковій упаковці",
     category: "Комп'ютерні мишки",
-    drop: "",
-    opt: "",
-    bulk: "",
+    drop: "150 грн",
+    opt: "3,50$",
+    bulk: "3,50$",
     image: "https://images.prom.ua/5857684400_w640_h640_igrovaya-kompyuternaya-myshka.jpg",
     availability: "В наявності"
   },
   {
     name: "Ігрова комп'ютерна мишка з підсвіткою М7 Кнопок",
     category: "Комп'ютерні мишки",
-    drop: "",
-    opt: "",
-    bulk: "",
+    drop: "170 грн",
+    opt: "3,8$",
+    bulk: "3,8$",
     image: "https://images.prom.ua/5857742748_w640_h640_igrovaya-kompyuternaya-myshka.jpg",
     availability: "В наявності"
   },
   {
     name: "Комп'ютерна Ігрова USB Миша V1",
     category: "Комп'ютерні мишки",
-    drop: "",
-    opt: "",
-    bulk: "",
+    drop: "100 грн",
+    opt: "2,2$",
+    bulk: "2,2$",
     image: "https://images.prom.ua/5857734217_w640_h640_kompyuternaya-igrovaya-usb.jpg",
     availability: "В наявності"
   }
