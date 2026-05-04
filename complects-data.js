e1e7f07e38ea7f279db811e99e55b46c023dd278
Artem <linequantum@gmail.com>
2026-05-04 17:12:37 +0300

    Fix complects text and availability

--- complects-data.js
+++ complects-data.js
@@ -1,362 +1,362 @@
-﻿const complectsProducts = [
+const complectsProducts = [
   {
-    name: "РљРѕРјРїР»РµРєС‚ A58Plus Р—РѕР»РѕС‚РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "450 РіСЂРЅ",
+    name: "Комплект A58Plus Золотий",
+    category: "Комплекти",
+    drop: "450 грн",
     opt: "10$",
     bulk: "10$",
     image: "images/complects/item-025.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ A58Plus РЎС–СЂРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "450 РіСЂРЅ",
+    name: "Комплект A58Plus Сірий",
+    category: "Комплекти",
+    drop: "450 грн",
     opt: "10$",
     bulk: "10$",
     image: "images/complects/item-026.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ A58Plus Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "450 РіСЂРЅ",
+    name: "Комплект A58Plus Чорний",
+    category: "Комплекти",
+    drop: "450 грн",
     opt: "10$",
     bulk: "10$",
     image: "images/complects/item-027.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ D21 Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "530 РіСЂРЅ",
+    name: "Комплект D21 Чорний",
+    category: "Комплекти",
+    drop: "530 грн",
     opt: "12$",
     bulk: "12$",
     image: "images/complects/item-028.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ D21 РЎС–СЂРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "530 РіСЂРЅ",
+    name: "Комплект D21 Сірий",
+    category: "Комплекти",
+    drop: "530 грн",
     opt: "12$",
     bulk: "12$",
     image: "images/complects/item-029.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ D21 РљРѕСЂРёС‡РЅРµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "530 РіСЂРЅ",
+    name: "Комплект D21 Коричневий",
+    category: "Комплекти",
+    drop: "530 грн",
     opt: "12$",
     bulk: "12$",
     image: "images/complects/item-030.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Infinity Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Infinity Чорний",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-016.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Infinity Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Infinity Білий",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-017.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Infinity Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Infinity Рожевий",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-018.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Watch9 suit Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Watch9 suit Чорний",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-019.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Watch9 suit Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Watch9 suit Білий",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-020.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Watch9 suit РџРѕРјР°СЂР°РЅС‡РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "400 РіСЂРЅ",
+    name: "Комплект Watch9 suit Помаранчевий",
+    category: "Комплекти",
+    drop: "400 грн",
     opt: "9$",
     bulk: "9$",
     image: "images/complects/item-021.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ M103 Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект M103 Чорний",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-031.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ M103 РЎС–СЂРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект M103 Сірий",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-032.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ M103 Р—РѕР»РѕС‚РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект M103 Золотий",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-033.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ WK76 Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект WK76 Чорний",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-034.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ WK76 РЎС–СЂРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект WK76 Сірий",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-035.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ WK76 Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "550 РіСЂРЅ",
+    name: "Комплект WK76 Рожевий",
+    category: "Комплекти",
+    drop: "550 грн",
     opt: "13$",
     bulk: "13$",
     image: "images/complects/item-036.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Megabox Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект Megabox Чорний",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-001.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Megabox Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект Megabox Білий",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-002.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ Megabox Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект Megabox Рожевий",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-003.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i50 suit max Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "600 РіСЂРЅ",
+    name: "Комплект i50 suit max Чорний",
+    category: "Комплекти",
+    drop: "600 грн",
     opt: "14$",
     bulk: "14$",
     image: "images/complects/item-004.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i50 suit max Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "600 РіСЂРЅ",
+    name: "Комплект i50 suit max Білий",
+    category: "Комплекти",
+    drop: "600 грн",
     opt: "14$",
     bulk: "14$",
     image: "images/complects/item-005.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i50 suit max Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "600 РіСЂРЅ",
+    name: "Комплект i50 suit max Рожевий",
+    category: "Комплекти",
+    drop: "600 грн",
     opt: "14$",
     bulk: "14$",
     image: "images/complects/item-006.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i60 suit extreme Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "650 РіСЂРЅ",
+    name: "Комплект i60 suit extreme Чорний",
+    category: "Комплекти",
+    drop: "650 грн",
     opt: "15$",
     bulk: "15$",
     image: "images/complects/item-007.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i60 suit extreme Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "650 РіСЂРЅ",
+    name: "Комплект i60 suit extreme Білий",
+    category: "Комплекти",
+    drop: "650 грн",
     opt: "15$",
     bulk: "15$",
     image: "images/complects/item-008.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ i60 suit extreme Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "650 РіСЂРЅ",
+    name: "Комплект i60 suit extreme Рожевий",
+    category: "Комплекти",
+    drop: "650 грн",
     opt: "15$",
     bulk: "15$",
     image: "images/complects/item-009.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ X8Plus Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "850 РіСЂРЅ",
+    name: "Комплект X8Plus Чорний",
+    category: "Комплекти",
+    drop: "850 грн",
     opt: "20$",
     bulk: "20$",
     image: "images/complects/item-010.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ X8Plus Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "850 РіСЂРЅ",
+    name: "Комплект X8Plus Білий",
+    category: "Комплекти",
+    drop: "850 грн",
     opt: "20$",
     bulk: "20$",
     image: "images/complects/item-011.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ X8Plus РџРѕРјР°СЂР°РЅС‡РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "850 РіСЂРЅ",
+    name: "Комплект X8Plus Помаранчевий",
+    category: "Комплекти",
+    drop: "850 грн",
     opt: "20$",
     bulk: "20$",
     image: "images/complects/item-012.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ T55 Pro Max Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "350 РіСЂРЅ",
+    name: "Комплект T55 Pro Max Чорний",
+    category: "Комплекти",
+    drop: "350 грн",
     opt: "8$",
     bulk: "8$",
     image: "images/complects/item-013.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ T55 Pro Max Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "350 РіСЂРЅ",
+    name: "Комплект T55 Pro Max Білий",
+    category: "Комплекти",
+    drop: "350 грн",
     opt: "8$",
     bulk: "8$",
     image: "images/complects/item-014.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "РќРµРјР°С” РІ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "Немає в наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ T55 Pro Max Р РѕР¶РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "350 РіСЂРЅ",
+    name: "Комплект T55 Pro Max Рожевий",
+    category: "Комплекти",
+    drop: "350 грн",
     opt: "8$",
     bulk: "8$",
     image: "images/complects/item-015.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ DT1000 Р§РѕСЂРЅРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект DT1000 Чорний",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-022.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ DT1000 Р‘С–Р»РёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект DT1000 Білий",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-023.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   },
   {
-    name: "РљРѕРјРїР»РµРєС‚ DT1000 РџРѕРјР°СЂР°РЅС‡РµРІРёР№",
-    category: "РљРѕРјРїР»РµРєС‚Рё",
-    drop: "700 РіСЂРЅ",
+    name: "Комплект DT1000 Помаранчевий",
+    category: "Комплекти",
+    drop: "700 грн",
     opt: "16$",
     bulk: "16$",
     image: "images/complects/item-024.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "Р’ РЅР°СЏРІРЅРѕСЃС‚С–"
+    availability: "В наявності"
   }
 ];
