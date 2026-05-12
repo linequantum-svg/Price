7cee5933a15c941a81a9bba26fa8c601944aadc2
Artem <linequantum@gmail.com>
2026-05-12 12:18:04 +0300

    Move larger powerbanks to top

--- powerbanks-data.js
+++ powerbanks-data.js
@@ -1,5 +1,77 @@
 ﻿const powerbanksProducts = [
   {
+    name: "Павербанк 20000 kj-C20 Білий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "480 грн",
+    opt: "11$",
+    bulk: "11$",
+    image: "images/powerbanks/powerbank-20000-white.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 20000 kj-C20 Чорний",
+    category: "Павербанки, зарядні пристрої",
+    drop: "480 грн",
+    opt: "11$",
+    bulk: "11$",
+    image: "images/powerbanks/powerbank-20000-black.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 20000 kj-C20 Бузковий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "480 грн",
+    opt: "11$",
+    bulk: "11$",
+    image: "images/powerbanks/powerbank-20000-lilac.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 20000 kj-C20 Рожевий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "480 грн",
+    opt: "11$",
+    bulk: "11$",
+    image: "images/powerbanks/powerbank-20000-pink-v2.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 30000 C20 Чорний",
+    category: "Павербанки, зарядні пристрої",
+    drop: "550 грн",
+    opt: "13$",
+    bulk: "13$",
+    image: "images/powerbanks/powerbank-30000-black.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 30000 C20 Білий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "550 грн",
+    opt: "13$",
+    bulk: "13$",
+    image: "images/powerbanks/powerbank-30000-white.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 30000 C20 Рожевий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "550 грн",
+    opt: "13$",
+    bulk: "13$",
+    image: "images/powerbanks/powerbank-30000-pink.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Павербанк 30000 C20 Бузьковий",
+    category: "Павербанки, зарядні пристрої",
+    drop: "550 грн",
+    opt: "13$",
+    bulk: "13$",
+    image: "images/powerbanks/powerbank-30000-lilac.jpg",
+    availability: "В наявності"
+  },
+  {
     name: "Павербанк 10000 kj-Z23 Білий",
     category: "Павербанки, зарядні пристрої",
     drop: "300 грн",
@@ -79,78 +151,6 @@
     bulk: "7$",
     image: "",
     availability: "Немає в наявності"
-  },
-  {
-    name: "Павербанк 20000 kj-C20 Білий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "480 грн",
-    opt: "11$",
-    bulk: "11$",
-    image: "images/powerbanks/powerbank-20000-white.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 20000 kj-C20 Чорний",
-    category: "Павербанки, зарядні пристрої",
-    drop: "480 грн",
-    opt: "11$",
-    bulk: "11$",
-    image: "images/powerbanks/powerbank-20000-black.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 20000 kj-C20 Бузковий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "480 грн",
-    opt: "11$",
-    bulk: "11$",
-    image: "images/powerbanks/powerbank-20000-lilac.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 20000 kj-C20 Рожевий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "480 грн",
-    opt: "11$",
-    bulk: "11$",
-    image: "images/powerbanks/powerbank-20000-pink-v2.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 30000 C20 Чорний",
-    category: "Павербанки, зарядні пристрої",
-    drop: "550 грн",
-    opt: "13$",
-    bulk: "13$",
-    image: "images/powerbanks/powerbank-30000-black.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 30000 C20 Білий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "550 грн",
-    opt: "13$",
-    bulk: "13$",
-    image: "images/powerbanks/powerbank-30000-white.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 30000 C20 Рожевий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "550 грн",
-    opt: "13$",
-    bulk: "13$",
-    image: "images/powerbanks/powerbank-30000-pink.jpg",
-    availability: "В наявності"
-  },
-  {
-    name: "Павербанк 30000 C20 Бузьковий",
-    category: "Павербанки, зарядні пристрої",
-    drop: "550 грн",
-    opt: "13$",
-    bulk: "13$",
-    image: "images/powerbanks/powerbank-30000-lilac.jpg",
-    availability: "В наявності"
   },
   {
     name: "Бездротове зарядне Fantasy",
