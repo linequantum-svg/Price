22cf0c5d68b96097addc5f5afe554a91aa45884d
Artem <linequantum@gmail.com>
2026-05-17 17:10:38 +0300

    Update D21 complects availability

--- complects-data.js
+++ complects-data.js
@@ -37,7 +37,7 @@ const complectsProducts = [
     bulk: "12$",
     image: "images/complects/item-028.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "В наявності"
+    availability: "Немає в наявності"
   },
   {
     name: "Комплект D21 Сірий",
@@ -47,7 +47,7 @@ const complectsProducts = [
     bulk: "12$",
     image: "images/complects/item-029.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "В наявності"
+    availability: "Немає в наявності"
   },
   {
     name: "Комплект D21 Коричневий",
@@ -57,7 +57,7 @@ const complectsProducts = [
     bulk: "12$",
     image: "images/complects/item-030.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "В наявності"
+    availability: "Немає в наявності"
   },
   {
     name: "Комплект Infinity Чорний",
