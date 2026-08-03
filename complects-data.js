3c51d81c8c1bbe08cd58d2dfee6984b2e573c137
Artem <linequantum@gmail.com>
2026-08-03 11:15:28 +0300

    Update D21 gray availability

--- complects-data.js
+++ complects-data.js
@@ -47,7 +47,7 @@ const complectsProducts = [
     bulk: "12$",
     image: "images/complects/item-029.png",
     availabilityUrl: "https://bit.ly/45isFp3",
-    availability: "В наявності"
+    availability: "Немає в наявності"
   },
   {
     name: "Комплект D21 Коричневий",
