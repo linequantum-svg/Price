8ec0943eab2932ed3f3f62c35e0da8fb3e987c75
Artem <linequantum@gmail.com>
2026-08-25 12:03:05 +0300

    Add Bajeal mice to catalog

--- mice-data.js
+++ mice-data.js
@@ -25,5 +25,50 @@ const miceProducts = [
     bulk: "2,2$",
     image: "https://images.prom.ua/5857734217_w640_h640_kompyuternaya-igrovaya-usb.jpg",
     availability: "В наявності"
+  },
+  {
+    name: "Мишка бездротова Bajeal Q20",
+    category: "Комп'ютерні мишки",
+    drop: "70 грн",
+    opt: "1,53$",
+    bulk: "1,53$",
+    image: "https://images.prom.ua/7658212076_w640_h640_myshka-besprovodnaya-bajeal.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Мишка Bajeal D12",
+    category: "Комп'ютерні мишки",
+    drop: "60 грн",
+    opt: "1,26$",
+    bulk: "1,26$",
+    image: "https://images.prom.ua/7656221957_w640_h640_provodnaya-opticheskaya-mysh.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Мишка ігрова Bajeal M500",
+    category: "Комп'ютерні мишки",
+    drop: "160 грн",
+    opt: "3,60$",
+    bulk: "3,60$",
+    image: "https://images.prom.ua/7656081376_w640_h640_besprovodnaya-igrovaya-mysh.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Мишка ігрова Bajeal G7",
+    category: "Комп'ютерні мишки",
+    drop: "150 грн",
+    opt: "3,47$",
+    bulk: "3,47$",
+    image: "https://images.prom.ua/7654991480_w640_h640_igrovaya-provodnaya-mysh.jpg",
+    availability: "В наявності"
+  },
+  {
+    name: "Мишка ігрова Bajeal G5",
+    category: "Комп'ютерні мишки",
+    drop: "110 грн",
+    opt: "2,70$",
+    bulk: "2,70$",
+    image: "https://images.prom.ua/7654991601_w640_h640_igrovaya-provodnaya-mysh.jpg",
+    availability: "В наявності"
   }
 ];
